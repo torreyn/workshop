62d4d8638939e30425604154977a6fae248180fa
Torrey <torrey@nommesen.com>
2013-10-27 22:47:30 -0700

    added iScroll

--- js/HomeView.js
+++ js/HomeView.js
@@ -6,11 +6,18 @@ var HomeView = function(store) {
         this.el.on('keyup', '.search-key', this.findByName);
     };
 
-    findByName: function() {
-        store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
+	this.findByName = function() {
+	    store.findByName($('.search-key').val(), function(employees) {
+	        $('.employee-list').html(HomeView.liTemplate(employees));
+	        if (self.iscroll) {
+	            console.log('Refresh iScroll');
+	            self.iscroll.refresh();
+	        } else {
+	            console.log('New iScroll');
+	            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+	        }
+	    });
+	};
 
 	this.initialize(); 
  }
