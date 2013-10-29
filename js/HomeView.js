01a321ab4388f2c433ac08197d80f55aa2e0f3c6
Torrey <torrey@nommesen.com>
2013-10-28 22:47:29 -0700

    Revert "added iScroll"
    This reverts commit 62d4d8638939e30425604154977a6fae248180fa.

--- js/HomeView.js
+++ js/HomeView.js
@@ -6,18 +6,11 @@ var HomeView = function(store) {
         this.el.on('keyup', '.search-key', this.findByName);
     };
 
-	this.findByName = function() {
-	    store.findByName($('.search-key').val(), function(employees) {
-	        $('.employee-list').html(HomeView.liTemplate(employees));
-	        if (self.iscroll) {
-	            console.log('Refresh iScroll');
-	            self.iscroll.refresh();
-	        } else {
-	            console.log('New iScroll');
-	            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
-	        }
-	    });
-	};
+    findByName: function() {
+        store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+    },
 
 	this.initialize(); 
  }
