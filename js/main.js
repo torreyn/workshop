9f881a3cf3c5dc87819345d896a6e03bcd0f23c4
Torrey <torrey@nommesen.com>
2013-10-28 23:11:05 -0700

    Revert "Part 6 of the lesson"
    This reverts commit d3b65e2b92462185e89d984feb8e5ede9e56f1aa.

--- js/main.js
+++ js/main.js
@@ -9,12 +9,25 @@ var app = {
         this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     },
 
+
+    findByName: function() {
+        var self = this;
+        this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+    },
+
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
             alert(title ? (title + ": " + message) : message);
         }
+    },
+
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
